d464cbf8be630205bd2107dbe863ebd9a3e9cea3
DenimCity <altidorj1@gmail.com>
2018-02-26 13:26:04 -0500

    added longs to the user model to appear in server

--- db/models/User.js
+++ db/models/User.js
@@ -2,4 +2,5 @@ const mongoose = require('mongoose')
 const Schema = require('../schema')
 const User = mongoose.model('User', Schema.UserSchema)
 
+console.log('hello from user schema')
 module.exports = User
