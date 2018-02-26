f365bedf4cea7c5a57541e286b63fb032619b8cb
DenimCity <altidorj1@gmail.com>
2018-02-26 13:25:47 -0500

    models are loggin in the server.. deleted the owner model

--- db/models/City.js
+++ db/models/City.js
@@ -2,4 +2,5 @@ const mongoose = require('mongoose')
 const Schema = require('../schema')
 const City = mongoose.model('City', Schema.CitySchema)
 
+console.log('hello from the city schema')
 module.exports = City
--- db/models/Owner.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose')
-const Schema = require('../schema')
-const Owner = mongoose.model('Owner', Schema.OwnerSchema)
-
-module.exports = Owner
