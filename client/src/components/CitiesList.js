6bd7a7a98fa5f7d623693525fcad972165d48e4c
DenimCity <altidorj1@gmail.com>
2018-02-26 17:13:37 -0500

    created new models

---
+++ client/src/components/CitiesList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CitiesList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hello from the cities list page</h1>
+      </div>
+    )
+  }
+}
