3644d161a7806171d63934921a81960cf440101d
DenimCity <altidorj1@gmail.com>
2018-02-26 13:24:46 -0500

    updated the schema files

--- db/schema.js
+++ db/schema.js
@@ -2,57 +2,115 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 mongoose.Promise = global.Promise
 
+console.log("hello from schema folder")
 
+const EventSchema = new Schema ({
+  owner: {
+    type: String
+  },
+  photo: {
+    type: String
+  },
+  phone: {
+    type: String
+  }
+}, {
+  timestamps: {},
+  usePushEach: true
+})
 
-const OwnerSchema = new Schema ({
-  owner: {type: String}, 
-  photo:{type: String},
- phone: {type: String}, 
-},{usePushEach: true},)
+const HouseSchema = new Schema({
+  house_photo: {
+    type: String
+  },
+  amenities: {
+    type: String
+  },
+  description: {
+    type: String
+  },
+  address: {
+    type: String
+  },
+  zip: {
+    type: String
+  },
+  price: {
+    type: String
+  },
+  kitchen: {
+    type: String
+  },
+  bathroom: {
+    type: String
+  },
+  livingroom: {
+    type: String
+  },
+  closet: {
+    type: String
+  },
+  backyard: {
+    type: String
+  },
+}, {
+  timestamps: {},
+  usePushEach: true
+},)
 
-const HouseSchema = new Schema ({
-  house_photo:{type: String},
-  amenities:{type: String}, 
-  description: {type: String}, 
-  address:{type: String},
-  zip: {type: String}, 
-  price:{type: String},
-  kitchen:{type: String},
-  bathroom:{type: String},
-  livingroom: {type: String},
-  closet:{type: String},
-  backyard:{type: String},
-  owners:[OwnerSchema] 
-},{usePushEach: true},)
-
-const CitySchema = new Schema ({
-  name: {type: String}, 
-  state:{type: String}, 
-  country:{type: String}, 
-  photo:{type: String},
+const CitySchema = new Schema({
+  name: {
+    type: String
+  },
+  state: {
+    type: String
+  },
+  country: {
+    type: String
+  },
+  photo: {
+    type: String
+  },
   houses: [HouseSchema]
 
-},{usePushEach: true},
-) 
+}, {
+  timestamps: {},
+  usePushEach: true
 
-const UserSchema = new Schema ({
-  name:{type: String},
-  email: {type: String},
-  password: {type: String},
-  photo: {type: String,default: 'http://www.clipartlord.com/wp-content/uploads/2014/04/egg4.png'},
-  degree:{type: String},
-  school:{type: String},
-  isHost:{type: Boolean},
-  
-},{usePushEach: true},
-)
+},)
 
-
-
+const UserSchema = new Schema({
+  name: {
+    type: String
+  },
+  email: {
+    type: String
+  },
+  password: {
+    type: String
+  },
+  photo: {
+    type: String,
+    default: 'http://www.clipartlord.com/wp-content/uploads/2014/04/egg4.png'
+  },
+  degree: {
+    type: String
+  },
+  school: {
+    type: String
+  },
+  isHost: {
+    type: Boolean
+  }
+}, {
+  timestamps: {},
+  usePushEach: true
+},)
 
 module.exports = {
   UserSchema,
   CitySchema,
-  HouseSchema,
-  OwnerSchema,
+  EventSchema,
+  HouseSchema
+  
 }
