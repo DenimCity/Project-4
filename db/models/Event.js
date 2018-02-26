3281ec5110d557b52ac60b8d81ec0c767ba40751
DenimCity <altidorj1@gmail.com>
2018-02-26 17:14:55 -0500

    commented out logs

--- db/models/Event.js
+++ db/models/Event.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose')
 const Schema = require('../schema')
 const Event = mongoose.model('Event', Schema.EventSchema)
 
-console.log('hello from the event house schema')
+// console.log('hello from the event house schema')
 
 module.exports = Event
--- db/models/House.js
+++ db/models/House.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const Schema = require('../schema')
 const House = mongoose.model('House', Schema.HouseSchema)
-console.log('hello from in the house schema')
+// console.log('hello from in the house schema')
 
 
 
