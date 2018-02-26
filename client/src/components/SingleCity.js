12048625c25f9687887397ec49c7177f7ffd502f
DenimCity <altidorj1@gmail.com>
2018-02-26 17:13:37 -0500

    created new models

---
+++ client/src/components/SingleCity.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SingleCity extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hello from the home page</h1>
+      </div>
+    )
+  }
+}
