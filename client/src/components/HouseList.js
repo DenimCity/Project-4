f459d72d5872b8a0763ef960fd143268c2bbeb34
DenimCity <altidorj1@gmail.com>
2018-02-27 15:23:37 -0500

    added house list component

---
+++ client/src/components/HouseList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class HouseList extends Component {
+  render() {
+    return (
+      <div>
+        hi from the house HouseList
+      </div>
+    )
+  }
+}
