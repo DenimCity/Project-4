b4b69b49c907f4b4e05ad5e92bb43799c319f94f
DenimCity <altidorj1@gmail.com>
2018-02-26 22:14:34 -0500

    component ready for styling of the home page

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
-
+import {Link} from 'react-router-dom'
+import styled from 'styled-components'
 export default class HomePage extends Component {
   render() {
     return (
-      <div>
-        <h1>hello from the home page</h1>
-      </div>
+      <HomeBody>
+        
+
+        
+      </HomeBody>
     )
   }
 }
+
+const HomeBody = styled.div`
+display:flex;
+flex-direction:column;
+
+
+`
