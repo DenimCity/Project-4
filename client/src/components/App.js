4fc49abe135252cbf2417bf83c54b2eb83c72f62
DenimCity <altidorj1@gmail.com>
2018-02-26 22:13:42 -0500

    api calles are maded to the server, obtained cities and user data

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,59 +1,74 @@
-import React, { Component } from 'react';
-import logo from '../logo.svg';
-import '../App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
 import axios from 'axios'
+//importing components
+import Homepage from './HomePage'
+import AboutPage from './AboutPage'
+import CitiesList from './CitiesList'
+import SingleCity from './SingleCity'
 
+//import styled components
+import NavBar from '../basicstyledcomponents/NavBar'
 class App extends Component {
   state = {
-    users:[],
+    users: {},
     cities:[],
-    events:[],
+    events: []
   }
 
-  userFromServer = () =>{
-    console.log('here from the user data route');
-    axios.get('/homecoming/user/profile').then(response =>{
-      const users = response.data
-      console.log(`the user data on the sever side, ${users}`)
-      this.setState({users})
-    })
+  userFromServer = () => {
+    // console.log('here from the user data route');
+    axios
+      .get('/homecoming/user/profile')
+      .then(response => {
+        const users = response.data
+        // console.log('the user data on the sever side', users)
+        this.setState({users})
+      })
   }
 
-
-  citiesFromServer = () =>{
-    console.log('here from the citie server api call')
-    axios.get('/homecoming/city/').then(response => {
-      const cities = response.data
-      console.log('cities data', cities)
-    })
+  citiesFromServer = () => {
+    // console.log(`here from the citie server api call`)
+    axios
+      .get('/homecoming/city/')
+      .then(response => {
+        const cities = response.data
+        // console.log('cities data', cities)
+        this.setState({cities})
+      })
   }
 
   eventsApi = () => {
-    console.log('here from third party api')
-    axios.get('').then(response => {
-      const events = response.data
-      this.setState({events})
-    })
+    // console.log(`here from third party api`)
+    axios
+      .get('')
+      .then(response => {
+        const events = response.data
+        this.setState({events})
+      })
   }
 
   componentWillMount = () => {
     this.userFromServer()
     this.citiesFromServer()
   }
-  
-
 
   render() {
+    const CityData = (props) => (<CitiesList CityInfo={this.state.cities}{...props}/>)
+    // console.log(`the city is being transfered'${CityData }`)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <NavBar/>
+      <Router>
+        <Switch>
+
+          <Route exact path="/" component={Homepage}/>
+          <Route exact path="/about" component={AboutPage} />
+          <Route exact path="/cities" component={CityData}/>
+          <Route exact path="/cities/cityId" component={SingleCity}/>
+
+        </Switch>
+      </Router>
       </div>
     );
   }
