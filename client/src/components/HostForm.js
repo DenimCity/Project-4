a04c313a7a8961b93006a02dcd75d1975c09afff
DenimCity <altidorj1@gmail.com>
2018-02-27 15:23:03 -0500

    created host form

---
+++ client/src/components/HostForm.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+export default class HostForm extends Component {
+  render() {
+    return (
+      <div>
+<form action="" method="get">
+<label htmlFor="">YEs</label>
+<div>
+<input type="text" placeholder="trust"/></div>
+
+
+</form>
+
+        
+      </div>
+    )
+  }
+}
