76fecd4fcfba323d890d0c4ff0bd85b86ac8ae27
DenimCity <altidorj1@gmail.com>
2018-02-28 18:09:18 -0500

    house list receiving the data, creating routes to create and delete house

--- client/src/components/HouseList.js
+++ client/src/components/HouseList.js
@@ -1,12 +1,70 @@
 import React, {Component} from 'react'
-
+import House from './House'
+import HouseForm from '../components/HouseForm'
+import axios from 'axios'
 export default class HouseList extends Component {
+
+  state = {
+    houses: []
+  }
+
+  componentWillMount() {
+    this.housesFromServer(this.props.match.params.cityId)
+  }
+
+  housesFromServer = (cityId) => {
+    console.log('from the houses route')
+    axios
+      .get(`/homecoming/city/${cityId}/houses`)
+      .then(response => {
+        const houses = response.data
+        console.log(`the houses data ${houses}`)
+        this.setState({houses})
+      })
+  }
+
+   createAHouse = async(cityId) => {
+    console.log('here from the create user route');
+    // const response = await axios.post(`/${cityId}/houses/new`)
+    const response = await axios.post(`/cityId/houses/new`, cityId)
+    //testing out destructing data
+    // const newHouse = {
+    //   name,
+    //   amenities,
+    //   description,
+    //   address,
+    //   zip,
+    //   price,
+    //   kitchen,
+    //   bathroom,
+    //   livingroom,
+    //   owner,
+    //   owner_photo,
+    //   owner
+    // } = response.data
+    const newHouse = response.data
+    const houses = [...this.state.houses]
+    houses.push(newHouse)
+    this.setState({houses})
+  }
+
+  deleteAHouse = async(houseId) => {
+    console.log('we hit the delete route')
+    const response = await axios.post()
+  }
+
   render() {
-    console.log('props', this.props.houses)
+    const makeNewHouse = () => (<HouseForm createHouse={this.createAHouse}/>)
+    const housesList = this
+      .state
+      .houses
+      .map((house, index) => {
+        return (<House key={index} house={house}/>)
+      })
     return (
       <div>
         <div>
-          <h1>{this.amenities}</h1>
+          house list! {housesList}
         </div>
       </div>
     )
