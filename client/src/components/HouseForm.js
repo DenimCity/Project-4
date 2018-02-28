5b7e645c1ff1a667e5e246e49df167933064a345
DenimCity <altidorj1@gmail.com>
2018-02-28 18:09:29 -0500

    creating house form

---
+++ client/src/components/HouseForm.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+export default class HouseForm extends Component {
+  render() {
+    return (
+      <div>
+        <form action="" method="get">
+          <label htmlFor="">YEs</label>
+          <div>
+            <input type="text" placeholder="trust"/></div>
+
+        </form>
+
+      </div>
+    )
+  }
+}
