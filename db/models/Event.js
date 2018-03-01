028cc2c55c9a1c8d65e9730bec0651b3bb84c9cc
DenimCity <altidorj1@gmail.com>
2018-02-28 23:19:49 -0500

    deleted longs

--- db/models/Event.js
+++ db/models/Event.js
@@ -1,7 +1,4 @@
 const mongoose = require('mongoose')
 const Schema = require('../schema')
 const Event = mongoose.model('Event', Schema.EventSchema)
-
-// console.log('hello from the event  schema')
-
 module.exports = Event
