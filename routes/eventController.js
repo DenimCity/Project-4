f1a3721a80507be7aaae84f962241eecf2f30469
DenimCity <altidorj1@gmail.com>
2018-02-28 23:18:13 -0500

    get and post route work, working on deleted and patch

--- routes/eventController.js
+++ routes/eventController.js
@@ -2,16 +2,56 @@ const express = require('express')
 const Event = require('../db/models/Event')
 const router = express.Router({mergeParams: true})
 
+// console.log(`hit from the event controller `);
 
-//get all events
-router.get('/', async(request, response) => {
-  console.log('hit the get all cities route')
+
+router.get('/', async (request, response)=>{
+console.log(`we hit the view app events routes`)
+try {
+  const events = await Event.find({})
+  response.json(events)
+} catch (error) {
+  console.log('could not obtain events',error)
+  response.json('postman: result error')
+  
+}
+})
+
+
+
+//post a new event 
+router.post('/create', async(request, response) => {
+  console.log(`hit the create event route`);
   try {
-    const cities = await City.find()
-    response.json(cities)
+      console.log('The data we are creating looks like => ', request.body)
+      const newEvent = await Event.create(request.body)
+      response.json(newEvent)
   } catch (error) {
-    console.log(`to view all the cities route error ${error}`);
+      console.log("Couldn't create user:", error)
+      response.sendStatus(500)
   }
+})
+
+
+router.delete('/:eventsId/delete', async (response, request)=> {
+  console.log(`here from the delete route`)
+  try {
+    const event = await Event.findByIdAndRemove(request.params.eventsId)
+    console.log('we grabbed teh event Id', event)
+    response.json(event)
+  } catch (error) {
+    console.log('could not delete',error )
+    // response.json('postman: delete error')
+    
+  }
+
+
+})
+
+
+router.patch('/eventId', async (response, request)=>{
+
+
 })
 
 
