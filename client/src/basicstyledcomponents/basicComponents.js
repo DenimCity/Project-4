8508da1a712b66ebc7435cbe4b4e69578044a45b
DenimCity <altidorj1@gmail.com>
2018-03-01 17:25:23 -0500

    created more styled componets

---
+++ client/src/basicstyledcomponents/basicComponents.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+
+export const Title = styled.h1`
+font-family:'Shadows Into Light', cursive;
+text-align:center;
+color:#C34C29;
+font-size:6rem;
+font-style:italic;
+`
+export const Title2 = styled.h2`
+font-family:'Shadows Into Light', cursive;
+text-align:center;
+color:goldenrod;
+font-size:3rem;
+font-style:italic;
+`
+
+export const Form = styled.form`
+  display : flex;
+  flex-direction: column;
+  justify-content: center;
+`
+export const Wrapper = styled.form`
+  display:flex;
+flex-direction:column;
+align-items:center;
+`
+
+export const Image = styled.img`
+border-radius:24px;
+
+`
