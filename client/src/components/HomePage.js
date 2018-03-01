313c2c270ee7a5aad88db9827171eff88f8271f9
DenimCity <altidorj1@gmail.com>
2018-03-01 17:23:37 -0500

    updated home page format

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react'
+import {Title } from '../basicstyledcomponents/basicComponents'
 import styled from 'styled-components'
 export default class HomePage extends Component {
   render() {
     return (
       <div>
+        <Title>Make finding your internship housing
+        </Title>
+        <Title>a piece of cake . . . </Title>
         <HomeBody>
 
           <img src="https://i.imgur.com/GhgBxx9.jpg" alt=""/>
@@ -18,5 +22,5 @@ const HomeBody = styled.div `
 display:flex;
 flex-direction:column;
 max-height:1rem;
-i
+
 `
