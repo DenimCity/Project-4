d2bd79c2665f4e526f9e3924357886b1723f26a0
DenimCity <altidorj1@gmail.com>
2018-03-01 17:27:14 -0500

    added page layout and info

--- client/src/components/AboutPage.js
+++ client/src/components/AboutPage.js
@@ -1,12 +1,57 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
+import {Title, Wrapper} from '../basicstyledcomponents/basicComponents'
+import styled from 'styled-components'
 export default class About extends Component {
   render() {
     return (
       <div>
-        <h1>hello from the about page</h1>
-      
+        <Title>About?</Title>
+        <Wrapper>
+          <Word>Sometimes finding housing for internships is a hassle
+          </Word>
+          <Word>But we just made it easier, we created this app to find homes</Word>
+          <Word>so students can find housing for 2 - 9 months.</Word>
+
+          <MediaContainer>
+            <div>
+              <GoLink href="https://twitter.com/JW_Altidor" target="_blank">
+                <Word>
+                  <i class="fab fa-twitter"></i>
+                </Word>
+              </GoLink>
+            </div>
+            <div>
+              <GoLink href="https://medium.com/@JW_Altidor" target="_blank">
+                <Word>
+                  <i class="fab fa-medium"></i>
+                </Word>
+              </GoLink>
+            </div>
+
+          </MediaContainer>
+        </Wrapper>
 
       </div>
     )
   }
 }
+
+const Word = styled.p `
+font-size:3.5rem;
+text-align:center;
+
+`
+
+const GoLink = styled.a `
+text-decoration:none;
+color:blue;
+&:hover{
+  color:red;
+}
+
+`
+
+const MediaContainer = styled.div`
+display:flex;
+
+`
