c71824cec14dd87692cfb8647e5d297522075cba
DenimCity <altidorj1@gmail.com>
2018-02-28 23:19:04 -0500

    deleted logs

--- db/models/Event.js
+++ db/models/Event.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose')
 const Schema = require('../schema')
 const Event = mongoose.model('Event', Schema.EventSchema)
 
-console.log('hello from the event  schema')
+// console.log('hello from the event  schema')
 
 module.exports = Event
