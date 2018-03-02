71a376ade9bd6e6c7dc9c1155de659f92cd65ec7
DenimCity <altidorj1@gmail.com>
2018-03-01 19:58:17 -0500

    LA route works

--- client/src/components/MeetUpApi/LaEventInfo.js
+++ client/src/components/MeetUpApi/LaEventInfo.js
@@ -1,11 +1,65 @@
-import React from 'react';
+import React, {Component} from 'react'
+import styled from 'styled-components'
+import { TableWrapper, Table, Column, ColumnTitle, Holder} from '../../basicstyledcomponents/basicComponents'
 
-const componentName = () => {
-  return (
-    <div>
-      
+export default class LaEventInfo extends Component {
+  render() {
+    return (
+      <div>
+      <div>
+        {this
+          .props
+          .laEvents
+          .map((event) => {
+            return (
+              <div>
+                <Table>
+                  <Column>
+                    <ColumnTitle>Name</ColumnTitle>
+                    {this
+                      .props
+                      .laEvents
+                      .map(event => {
+                        return <Holder>{event.name}</Holder>
+                      })}
+                  </Column>
+                  <Column>
+                    <ColumnTitle>Number of Members</ColumnTitle>
+                    {this
+                      .props
+                      .laEvents
+                      .map(event => {
+                        return <Holder>{event.members}</Holder>
+                      })}
+                  </Column>
+                  <Column>
+                    <ColumnTitle>Categories</ColumnTitle>
+                    {this
+                      .props
+                      .laEvents
+                      .map(event => {
+                        return <Holder>{event.category.name}</Holder>
+                      })}
+                  </Column>
+
+                  <Column>
+                    <ColumnTitle>Link</ColumnTitle>
+                    {this
+                      .props
+                      .laEvents
+                      .map(event => {
+                        return <Holder>
+                          <a href={event.link} target="blank">
+                            Click Here</a>
+                        </Holder>
+                      })}
+                  </Column>
+                </Table>
+              </div>
+            )
+          })}
+      </div>
     </div>
-  );
-};
-
-export default componentName;
+    )
+  }
+}
