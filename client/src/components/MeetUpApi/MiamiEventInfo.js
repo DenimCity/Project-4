b4e4028c5d9a2aee6b0f00bf59db76fa7571958c
DenimCity <altidorj1@gmail.com>
2018-03-01 19:35:09 -0500

    created miami component and ins now rendering

---
+++ client/src/components/MeetUpApi/MiamiEventInfo.js
@@ -0,0 +1,67 @@
+import React, {Component} from 'react'
+import styled from 'styled-components'
+import { TableWrapper, Table, Column, ColumnTitle, Holder} from '../../basicstyledcomponents/basicComponents'
+
+
+export default class MiamiEventInfo extends Component {
+  render() {
+    return (
+      <div>
+      <div>
+        {this
+          .props
+          .miamiEvents
+          .map((event) => {
+            return (
+              <div>
+
+                <Table>
+                  <Column>
+                    <ColumnTitle>Name</ColumnTitle>
+                    {this
+                      .props
+                      .miamiEvents
+                      .map(event => {
+                        return <Holder>{event.name}</Holder>
+                      })}
+                  </Column>
+                  <Column>
+                    <ColumnTitle>Number of Members</ColumnTitle>
+                    {this
+                      .props
+                      .miamiEvents
+                      .map(event => {
+                        return <Holder>{event.members}</Holder>
+                      })}
+                  </Column>
+                  <Column>
+                    <ColumnTitle>Categories</ColumnTitle>
+                    {this
+                      .props
+                      .miamiEvents
+                      .map(event => {
+                        return <Holder>{event.category.name}</Holder>
+                      })}
+                  </Column>
+
+                  <Column>
+                    <ColumnTitle>Link</ColumnTitle>
+                    {this
+                      .props
+                      .miamiEvents
+                      .map(event => {
+                        return <Holder>
+                          <a href={event.link} target="blank">
+                            Click Here</a>
+                        </Holder>
+                      })}
+                  </Column>
+                </Table>
+              </div>
+            )
+          })}
+      </div>
+    </div>
+    )
+  }
+}
