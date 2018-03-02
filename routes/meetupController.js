834966083a78d0eb485aac5ea43440acd156dd2a
DenimCity <altidorj1@gmail.com>
2018-03-01 19:57:11 -0500

    removed white spaces

--- routes/meetupController.js
+++ routes/meetupController.js
@@ -1,14 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const axios = require('axios')
-router.get('/miami', async(request, response) => {
-  // console.log('hit the miami controller ')
 
+router.get('/miami', async(request, response) => {
   axios
     .get(`https://api.meetup.com/find/groups?key=${process.env.EVENT_KEY}&photo-host=public&location=Miami&page=2&sig_id=245595678&sig=8daa81f3cdd70065320c6561bea8addc5398f08e`)
     .then((response)=> {
       return response.data
-      
     }).then((miami)=> {
       response.json(miami)
     }).catch((error)=>{
@@ -17,8 +15,6 @@ router.get('/miami', async(request, response) => {
 
 })
 router.get('/atlanta', async(request, response) => {
-  // console.log('hit the meetup controller ')
-
   axios
     .get(`https://api.meetup.com/find/groups?key=${process.env.EVENT_KEY}&photo-host=public&location=Atlanta%2CGa&page=3&sig_id=245595678&sig=6cf7a038a4f9a98787a82fcf8aae96915e2a55eb`)
     .then((response)=> {
@@ -28,13 +24,11 @@ router.get('/atlanta', async(request, response) => {
     }).catch((error)=>{
       console.log('no data found',error);
     })
-
 })
-router.get('/la', async(request, response) => {
-  console.log('hit the meetup controller ')
 
-  axios
-    .get(`https://api.meetup.com/find/groups?key=${process.env.EVENT_KEY}&photo-host=public&location=los+angeles&page=1&sig_id=245595678&sig=3794f40556d38e63ff33b89439c6aa52680d9cbb`)
+router.get('/la', async(request, response) => {
+ axios
+    .get(`https://api.meetup.com/find/groups?key=${process.env.EVENT_KEY}&photo-host=public&location=los+angeles&page=3&sig_id=245595678&sig=3794f40556d38e63ff33b89439c6aa52680d9cbb`)
     .then((response)=> {
       return response.data
     }).then((la)=> {
@@ -42,11 +36,9 @@ router.get('/la', async(request, response) => {
     }).catch((error)=>{
       console.log('no data found',error);
     })
-
 })
-router.get('/newyork', async(request, response) => {
-  // console.log('hit the meetup controller ')
 
+router.get('/newyork', async(request, response) => {
   axios
     .get(`https://api.meetup.com/find/groups?key=${process.env.EVENT_KEY}&photo-host=public&location=New+York&page=4&sig_id=245595678&sig=84e56927e5bce3de07c37ad434c0dd30a91ef1f1`)
     .then((response)=> {
