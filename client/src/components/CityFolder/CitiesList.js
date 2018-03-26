f6c4d6fda931f2070f1e2bb4cfb0b4ab08cbe490
DenimCity <altidorj1@gmail.com>
2018-03-26 14:32:42 -0400

    moved api call in this component and added loading screen

--- client/src/components/CityFolder/CitiesList.js
+++ client/src/components/CityFolder/CitiesList.js
@@ -1,11 +1,40 @@
 import React, {Component} from 'react'
 import City from '../CityFolder/City'
-
+import axios from 'axios'
+import styled from 'styled-components'
 export default class CitiesList extends Component {
+  state = {
+    cities: [],
+    isLoading: true
+  }
+
+  citiesFromServer = () => {
+    axios
+      .get(`/homecoming/city/`)
+      .then(response => {
+        const cities = response.data
+        this.setState({cities, isLoading: false})
+      })
+  }
+  componentWillMount() {
+    setTimeout(() => {
+      this.citiesFromServer()
+    }, 300);
+
+  }
+
   render() {
+    if (this.state.isLoading) {
+      return <Loading>
+        <img
+          src="https://loading.io/spinners/ellipsis/lg.discuss-ellipsis-preloader.gif"
+          alt=""/>
+      </Loading>
+    }
+
     const cityList = this
-      .props
-      .CityInfo
+      .state
+      .cities
       .map((city, index) => {
         return (<City
           key={index}
@@ -23,3 +52,10 @@ export default class CitiesList extends Component {
     )
   }
 }
+
+const Loading = styled.div `
+display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+
+`
