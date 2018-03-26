3236b29cc1873c97a4f6dd6199952e8c03e058ef
DenimCity <altidorj1@gmail.com>
2018-03-26 14:19:03 -0400

    added loading element for bettering user experience while loading api

--- client/src/components/MeetUpApi/MeetUpApiCall.js
+++ client/src/components/MeetUpApi/MeetUpApiCall.js
@@ -5,7 +5,7 @@ import LaEventInfo from '../MeetUpApi/LaEventInfo'
 import MiamiEventInfo from '../MeetUpApi/MiamiEventInfo'
 import NewYorkEventInfo from '../MeetUpApi/NewYorkEventInfo'
 import {Title} from '../../basicstyledcomponents/basicComponents'
-
+import styled from 'styled-components'
 export default class MeetUpApiCalls extends Component {
 
   state = {
@@ -13,7 +13,7 @@ export default class MeetUpApiCalls extends Component {
     newYorkEvents: [],
     miamiEvents: [],
     laEvents: [],
-    isLoaded: false
+    isLoading: true
   }
 
   atlantaApiCall = () => {
@@ -21,8 +21,7 @@ export default class MeetUpApiCalls extends Component {
       .get('homecoming/meetups/atlanta')
       .then(response => {
         const atlEvents = response.data
-        // console.log('data fora atlanta ', atlEvents )
-        this.setState({atlEvents, isLoaded: true})
+        this.setState({atlEvents, isLoading: false})
       })
 
   }
@@ -31,7 +30,7 @@ export default class MeetUpApiCalls extends Component {
       .get('/homecoming/meetups/newyork')
       .then(response => {
         const newYorkEvents = response.data
-        this.setState({newYorkEvents, isLoaded: true})
+        this.setState({newYorkEvents, isLoading: false})
       })
 
   }
@@ -40,8 +39,7 @@ export default class MeetUpApiCalls extends Component {
       .get('/homecoming/meetups/miami')
       .then(response => {
         const miamiEvents = response.data
-        // console.log('data fora miami ', miamiEvents )
-        this.setState({miamiEvents, isLoaded: true})
+        this.setState({miamiEvents, isLoading: false})
       })
 
   }
@@ -50,40 +48,47 @@ export default class MeetUpApiCalls extends Component {
       .get('homecoming/meetups/la')
       .then(response => {
         const laEvents = response.data
-        // console.log('data fora los angeles ', laEvents )
         this.setState({laEvents})
       })
 
   }
   componentWillMount = () => {
-
-    this.atlantaApiCall()
-    this.newYorkApiCall()
-    this.miamiApiCall()
-    this.losAngelesApiCall()
+    setTimeout(() => {
+      this.atlantaApiCall()
+      this.newYorkApiCall()
+      this.miamiApiCall()
+      this.losAngelesApiCall()
+    }, 900);
 
   }
 
   render() {
-    const {isLoaded} = this.props
 
+    if (this.state.isLoading) {
+      return <Loading>
+        <img
+          src="https://loading.io/spinners/ellipsis/lg.discuss-ellipsis-preloader.gif"
+          alt=""/>
+      </Loading>
+    }
     return (
       <div>
-
-        {!isLoaded
-          ? <div>
-              <Title>ATL Events</Title>
-              <AtlEventInfo atlEvents={this.state.atlEvents}/>
-              <Title>NY Events</Title>
-              <NewYorkEventInfo newYorkEvents={this.state.newYorkEvents}/>
-              <Title>MIA Events</Title>
-              <MiamiEventInfo miamiEvents={this.state.miamiEvents}/>
-              <Title>LA Events</Title>
-              <LaEventInfo laEvents={this.state.laEvents}/>
-            </div>
-          : <img src="https://loading.io/spinners/ellipsis/lg.discuss-ellipsis-preloader.gif" alt=""/>}
-
+        <Title>ATL Events</Title>
+        <AtlEventInfo atlEvents={this.state.atlEvents}/>
+        <Title>NY Events</Title>
+        <NewYorkEventInfo newYorkEvents={this.state.newYorkEvents}/>
+        <Title>MIA Events</Title>
+        <MiamiEventInfo miamiEvents={this.state.miamiEvents}/>
+        <Title>LA Events</Title>
+        <LaEventInfo laEvents={this.state.laEvents}/>
       </div>
     )
   }
 }
+
+const Loading = styled.div `
+display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+
+`
