6b0590304666dd20beef7fbe9007ed00b45c8a6c
DenimCity <altidorj1@gmail.com>
2018-04-01 17:12:00 -0400

    added toggle features, email and phone number is now clickable.

--- client/src/components/HouseFolder/House.js
+++ client/src/components/HouseFolder/House.js
@@ -4,39 +4,51 @@ import styled from 'styled-components'
 export default class House extends Component {
 
   state = {
-    ownerShowing: false
+    ownerShowing: true
   }
 
-  handleClick = (event) => {
-
+  handleClick(event) {
     this.toggleOwner()
   }
+
   toggleOwner = () => {
-    const ownerShowing = !this.state.ownerShowing
-    // this.setState({ownerShowing})
+    const {ownerShowing} = this.state
+    this.setState({
+      ownerShowing: !ownerShowing
+    })
   }
 
   render() {
     const {house} = this.props
+    if (this.state.ownerShowing) {
+      return (
+        <OwnerCard className="OwnerCard">
+          <TextAlign>
+            <HouseContainer>
+              <HousePhoto src={house.house_photo} alt="house"/>
+            </HouseContainer>
+            <div>
+              <strong>Description</strong>
+            </div>
+            <div>{house.description}</div>
+            <strong>Amenities</strong>
+            <div>{house.amenities}</div>
+            <strong>Address</strong>
+            <div>{house.address}</div>
+            <strong>Price</strong>
+            <div>{house.price}/month</div>
+            <br/>
+            <button onClick={() => this.toggleOwner()}>View Owner</button>
+            {/* <a href={`/cities/:cityId/houses/:${house._id}`} >View Owner</a> */}
+          </TextAlign>
+        </OwnerCard>
+      )
+    }
     return (
       <OwnerCard className="OwnerCard">
-        <TextAlign>
-          <HouseContainer>
-            <img width="100%" src={house.house_photo} alt="house"/>
-          </HouseContainer>
-          <div>
-            <strong>Description</strong>
-          </div>
-          <div>{house.description}</div>
-          <strong>Amenities</strong>
-          <div>{house.amenities}</div>
-          <strong>Address</strong>
-          <div>{house.address}</div>
-          <div>{house.price}/month</div>
-        </TextAlign>
-        
-        <img width="100%" height="400px"  src={house.owner_photo} alt="owner"/>
-        
+
+        <OwnerPhoto src={house.owner_photo} alt="owner"/>
+
         <OwnerInfoWrapper className="ownerInfoWrapper">
           <Container className="container">
             <div>
@@ -51,8 +63,14 @@ export default class House extends Component {
               <strong>Contact</strong>
             </div>
             <div>
-              <div>{house.owner_phone}</div>
+              <div>
+                <a href={`tel:+${house.owner_phone}`}>Call</a>
+              </div>
             </div>
+            <div>
+              <a href={`mailto:${house.owner_email}?Subject=House%20Posting`}>eMail</a>
+            </div>
+            <button onClick={() => this.toggleOwner()}>View House</button>
           </Container>
         </OwnerInfoWrapper>
       </OwnerCard>
@@ -80,13 +98,32 @@ text-align:center;
 const OwnerCard = styled.div `
 border: 1px solid;
 box-shadow: 2px 3px 3px 3px grey;
+max-width:500px;
+height: 45vh;
+@media (max-width: 669px){
+  height: 60vh;
+}
 `
 
-const HouseContainer = styled.div`
+const HouseContainer = styled.div `
 /* width:10vw; */
 
 `
 
+const OwnerPhoto = styled.img `
+width:100%; 
+height:30vh;
+@media (max-width: 669px){
+  width: 100%;
+}
+
+`
+
+const HousePhoto = styled.img `
+width:100%;
+height:30vh;
+
+`
 
 //keys to use later
 {/* <div><img width="100%" src={house.kitchen} alt=""/></div>
