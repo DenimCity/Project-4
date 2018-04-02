e813604689246f69457fa91cc6a652bc4d3200da
DenimCity <altidorj1@gmail.com>
2018-04-02 11:49:58 -0400

    removed state, user axios call, signin page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-import axios from 'axios'
 //importing components
 import Homepage from './HomePage'
 import AboutPage from './AboutPage'
@@ -9,26 +8,10 @@ import HouseList from '../components/HouseFolder/HouseList'
 import HouseForm from '../components/HouseFolder/HouseForm'
 import MeetUpApiCall from './MeetUpApi/MeetUpApiCall'
 import HomePending from '../components/HouseFolder/HomePending'
-import SignIn from './SignIn'
 
 //import styled components
 import NavBar from '../basicstyledcomponents/NavBar'
 class App extends Component {
-  state = {
-    users: {}
-  }
-
-  userFromServer = () => {
-    axios
-      .get('/homecoming/user/profile')
-      .then(response => {
-        const users = response.data
-        this.setState({users})
-      })
-  }
-  componentWillMount = () => {
-    this.userFromServer()
-  }
 
   render() {
     return (
@@ -42,7 +25,6 @@ class App extends Component {
             <Route exact path="/cities/:cityId/houses" component={HouseList}/>
             <Route exact path="/meetups" component={MeetUpApiCall}/>
             <Route exact path="/hosting" component={HouseForm}/>
-            <Route exact path="/signin" component={SignIn}/>
             <Route exact path="/housepending" component={HomePending}/>
           </Switch>
         </Router>
