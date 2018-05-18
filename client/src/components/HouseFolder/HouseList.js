7c1758c9df80111528eace3b4707913ea6b054ad
E Altidor <ealtidor@macbook-pro.attlocal.net>
2018-05-18 00:27:27 -0400

    added house list

---
+++ client/src/components/HouseFolder/HouseList.js
@@ -0,0 +1,80 @@
+import React, {Component} from 'react'
+import House from './House'
+import HouseForm from './HouseForm'
+import styled from 'styled-components'
+import axios from 'axios'
+
+export default class HouseList extends Component {
+
+  state = {
+    houses: []
+  }
+
+  componentWillMount() {
+    this.housesFromServer(this.props.match.params.cityId)
+  }
+
+  housesFromServer = (cityId) => {
+    axios
+      .get(`/homecoming/city/${cityId}/houses`)
+      .then(response => {
+        const houses = response.data
+        this.setState({houses})
+      })
+  }
+
+   createAHouse = async(cityId) => {
+    console.log('here from the create user route');
+    const response = await axios.post(`/cityId/houses/new`, cityId)
+    const newHouse = response.data
+    const houses = [...this.state.houses]
+    houses.push(newHouse)
+    this.setState({houses})
+  }
+// working on delete route
+  //
+  // deleteAHouse = async(houseId) => {
+  //   console.log('we hit the delete route')
+  //   const response = await axios.post()
+  // }
+
+  render() {
+    const makeNewHouse = () => (<HouseForm createHouse={this.createAHouse}/>)
+    const housesList = this
+      .state
+      .houses
+      .map((house, index) => {
+        return (<House key={index} house={house}/>)
+      })
+    return (
+      <HouseWrapper className="houeWrapper" >
+        <HouseContainer className="HoueContainer">
+          {housesList}
+        </HouseContainer>
+      </HouseWrapper>
+    )
+  }
+}
+
+
+const HouseContainer = styled.div`
+display:grid;
+grid-template-columns:auto auto;
+max-width: 63.2rem;
+    grid-gap: 2rem;
+    padding: 3rem 0rem;
+
+@media (max-width: 669px) {
+  grid-template-columns:auto;
+padding:0rem;
+  }
+
+`
+
+const HouseWrapper = styled.div`
+display:flex;
+flex-wrap:wrap;
+justify-content: center;
+flex-direction:row;
+
+`
